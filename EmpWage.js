d24d5a656636282ef2df0b944e2551f54a35243f
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-07-18 10:37:17 +0530

    Added code to check emp daily wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,9 +1,21 @@
 console.log("Employee Wage Computation")
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOUR = 4;
+const FULL_TIME_HOUR = 8;
+const WAGE_PER_HOUR = 20;
+let empHours = 0 ;
 const IS_ABSENT = 0;
 let empCheck = Math.floor(Math.random() * 10) % 2;
-if(empCheck == IS_ABSENT) {
-    console.log("emp is absent");
-    return;
-}else {
-    console.log("emp is present");
+switch(empCheck) {
+    case IS_PART_TIME :
+            empHours = PART_TIME_HOUR;
+            break;
+    case IS_FULL_TIME :
+            empHours = FULL_TIME_HOUR;
+            break;
+    default :
+            empHours = 0;
 }
+let empWage = empHours * WAGE_PER_HOUR;
+console.log("emp wage is " + empWage);

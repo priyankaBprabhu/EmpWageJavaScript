93939dfdc4f81e6dbe7e7406e197163c14d6a7b9
priyankaBprabhu <priyankaprabhu96@gmail.com>
2022-07-18 10:31:14 +0530

    Added code to check emp is persent or absent

--- EmpWage.js
+++ EmpWage.js
@@ -1,1 +1,9 @@
 console.log("Employee Wage Computation")
+const IS_ABSENT = 0;
+let empCheck = Math.floor(Math.random() * 10) % 2;
+if(empCheck == IS_ABSENT) {
+    console.log("emp is absent");
+    return;
+}else {
+    console.log("emp is present");
+}
